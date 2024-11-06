d6633c98904a1044567eaa26500684824e9680eb
hyunsu jang <jhsnex3768@gmail.com>
2024-11-07 08:07:37 +0900

    Comment 추가
    Signed-off-by: hyunsu jang <jhsnex3768@gmail.com>

---
+++ src/main/java/com/example/firstproject/service/CommentService.java
@@ -0,0 +1,37 @@
+package com.example.firstproject.service;
+
+import com.example.firstproject.dto.CommentDto;
+import com.example.firstproject.entity.Comment;
+import com.example.firstproject.repository.ArticleRepository;
+import com.example.firstproject.repository.CommentRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.stereotype.Service;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+
+@Service
+public class CommentService {
+    @Autowired
+    private CommentRepository commentRepository;
+    @Autowired
+    private ArticleRepository articleRepository;
+
+    public List<CommentDto> comments(Long articleId) {
+        // 1. 댓글 조회
+        List<Comment> comments = commentRepository.findByArticleId(articleId);
+        // 2. 엔티티 -> DTO 변환
+        List<CommentDto> dtos = new ArrayList<CommentDto>();
+        for(int i = 0; 1< comments.size(); i++) {
+            Comment comment = comments.get(i);
+            CommentDto dto = CommentDto.createCommentDto(comment);
+            dtos.add(dto);
+        }
+        // 3. 결과 반환
+        return dtos;
+    }
+}
